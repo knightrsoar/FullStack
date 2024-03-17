58891f0e9a3953c21b30b7aebc2053428a863bbe
YuyanPeng <112590847+knightrsoar@users.noreply.github.com>
2024-03-17 19:12:17 -0400

    Add files via upload

---
+++ eShopAssignment/ApplicationCore/Interfaces/Service/IRegionServiceAsync.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Model.Request;
+using ApplicationCore.Model.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.Service
+{
+    public interface IRegionServiceAsync
+    {
+        Task<RegionResponseModel> GetAllRegions();
+        Task<int> AddNewRegion(RegionRequestModel r);
+        Task<int> GetRegionCountAsync();
+        Task<IEnumerable<RegionResponseModel>> GetRegionByName(string name);
+        
+
+    }
+}
---
+++ eShopAssignment/ApplicationCore/Model/Response/RegionResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Model.Response
+{
+    public class RegionResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
